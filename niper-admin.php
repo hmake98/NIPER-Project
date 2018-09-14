0e22e4ddac6e8a1a3ba5e6074ee6eb4962136b12
hardikpanchal26 <hardikpanchal551@gmail.com>
2018-09-14 21:02:52 +0530

    Niper Admin Layout

--- niper-admin.php
+++ niper-admin.php
@@ -1,42 +1,127 @@
 <?php include 'layouts/master_layout_top.php'; ?>
+<!--
+  <div class="row justify-content-center" >
+      <div class="pr-5 pl-5" style="border:2px solid #f2f2f2; width: 400px">
+      <form>
 
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <span class="input-group-text" id="basic-addon1">@</span>
-  </div>
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-</div>
+       <h3 align="center" class="mb-4 mt-4">NIPER Admin Login</h3>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" ><i class="fa fa-user"></i></span>
+        </div>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      </div>
 
-<div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-  <div class="input-group-append">
-    <span class="input-group-text" id="basic-addon2">@example.com</span>
-  </div>
-</div>
+      <div class="input-group mb-4">
+        <div class="input-group-prepend">
+          <span class="input-group-text"><i class="fa fa-keyboard-o"></i></span>
+        </div>
+        <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+      </div>
 
-<label for="basic-url">Your vanity URL</label>
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
+      <div class="input-group mb-3">
+        <input type="submit" class="form-control btn btn-primary" />
+      </div>
+      </form>
+      </div>
   </div>
-  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-</div>
+-->
+  <div class="row justify-content-center" >
+      <div class="pr-5 pl-5" style="border:2px solid #f2f2f2; width: 1000px; background: #f2f2f2">
+      <form>
 
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <span class="input-group-text">$</span>
+       <h3 align="center" class="mb-4 mt-4">Add New Instrument</h3>
+      <span>Add a new instrument.</span>
+      <div class="input-group mb-5">
+        <div class="input-group-prepend">
+          <span class="input-group-text" ><i class="fa fa-wrench"></i></span>
+        </div>
+        <input type="text" class="form-control" placeholder="Instrument" style="width:40%"> 
+        <button type="submit" class="form-control btn btn-primary ml-2">Add</button>
+      </div>
+      </form>
+      </div>
   </div>
-  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-  <div class="input-group-append">
-    <span class="input-group-text">.00</span>
-  </div>
-</div>
 
-<div class="input-group">
-  <div class="input-group-prepend">
-    <span class="input-group-text">With textarea</span>
+<br><br>
+   <div class="row justify-content-center" >
+      <div class="pr-5 pl-5" style="border:2px solid #f2f2f2; width: 1000px; background: #f2f2f2">
+      <form>
+
+       <h3 align="center" class="mb-4 mt-4">Add Instrument Facilities</h3>
+       <br>
+       <span>Select Instrument and Add Facility.</span>  
+       <div class="row">
+        <div class="col-md-6" style="padding-right: 5px">
+          <div class="input-group mb-4">
+            <div class="input-group-prepend">
+                <span class="input-group-text" ><i class="fa fa-wrench"></i></span>
+            </div>
+            <select class="custom-select form-control">
+              <option selected>-- Select Instrument --</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="col-md-6" style="padding-left: 5px">
+          <div class="input-group mb-4">
+            <div class="input-group-prepend">
+              <span class="input-group-text" ><i class="fa fa-flask"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Facility"> 
+          </div>
+        </div>
+      </div>
+
+      
+
+      <div class="row">
+        <div class="col-md-6" style="padding-right: 5px">
+          <div class="input-group mb-4">
+            <div class="input-group-prepend">
+              <span class="input-group-text" ><i class="fa fa-rupee"></i></span>
+             </div>
+            <input type="text" class="form-control" placeholder="Charge for Insdustry">
+          </div>
+        </div>
+        
+        <div class="col-md-6" style="padding-left: 5px">
+          <div class="input-group mb-4">
+            <div class="input-group-prepend">
+              <span class="input-group-text" ><i class="fa fa-rupee"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Charge for Institutes (Govt. and Private)"> 
+           </div>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-md-6" style="padding-right: 5px">
+          <div class="input-group mb-5">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >Remarks</span>
+            </div>
+            <input type="text" class="form-control" >
+          </div>
+        </div>
+
+        <div class="col-md-6" style="padding-left: 5px">
+          <div class="input-group mb-5">
+              <input type="submit" class="form-control btn btn-primary" value="Add"> 
+          </div>
+        </div>
+      </div>
+
+
+
+
+      </form>
+      </div>
   </div>
-  <textarea class="form-control" aria-label="With textarea"></textarea>
-</div>
+
 
 <?php include 'layouts/master_layout_bottom.php'; ?>
